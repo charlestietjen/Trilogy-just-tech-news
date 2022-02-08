a062419a53aee3b3ce228a69bbae68b5bae823a0
Charles T <tietjen.charles@gmail.com>
2022-02-08 15:16:03 -0500

    hashed passwords

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes/index');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -8,8 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
